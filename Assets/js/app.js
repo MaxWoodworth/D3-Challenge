e7732009562e7656bfe5224e9e53926de2adb80a
Max Woodworth <max10woodworth@gmail.com>
2021-02-18 02:21:41 -0600

    scales and appends axis'

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -19,3 +19,37 @@ var svg = d3.select("#scatter")
     .attr("h", svgHeight)
     .attr("w", svgWidth);
 
+var chartGroup = svg.append("g")
+    .attr("transofrm", 'translate(${margin.left}, ${margin.top})');
+
+//Get data from csv
+d3.csv("data.csv").then(function (data){
+    //parse
+    obage.forEach(function (data){
+        data.age = +data.age;
+        data.obesity = +data.obesity;   
+    });
+    
+    //Scale Function using xlinearscale
+    var xLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(
+            data, d => d.age)])
+            .range([w]);
+    
+            //Scale Function using ylinearscale
+    var yLinearScale = d3.scaleLinear()
+    .domain([0,d3.max(data, d => d.obesity)])
+    .range([height, 0]);
+    
+    //Axis functions with Linear scale
+    var bottomaxis = d3.axisBottom(xLinearScale);
+    var leftaxis = d3.axisLeft(yLinearScale);
+    
+    //apend both axis
+    chartGroup.append("g")
+    .attr("transform", 'translate(0, ${height})')
+    .call(bottomaxis);
+    chartGroup.append("g")
+    .call(leftaxis);
+
+})
